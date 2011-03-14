c7d723de1528a00c73ac20d50d4473d6c65dc961
Chris Streeter <chris@chrisstreeter.com>
2011-03-13 19:12:28 -0700

    It can be uploaded to PyPi now!

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup, find_packages
+    from setuptools.command.test import test
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+    from setuptools.command.test import test
+
+
+class mytest(test):
+    def run(self, *args, **kwargs):
+        from runtests import runtests
+        runtests()
+        # Upgrade().run(dist=True)
+        # test.run(self, *args, **kwargs)
+
+setup(
+    name='django-ticketing',
+    version='0.1.0',
+    author='Chris Streeter',
+    author_email='chris@chrisstreeter.com',
+    url='http://github.com/streeter/django-ticketing',
+    description = 'Generate tickets efficiently in a database in Django',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+    ],
+    test_suite = 'ticketing.tests',
+    include_package_data=True,
+    cmdclass={"test": mytest},
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ],
+)
