58fbd1ace9052d816b20907b060a870d248a7a2b
Chris Streeter <chris@chrisstreeter.com>
2011-09-13 09:22:03 -0700

    Fix package distribution.

--- ticketing/__init__.py
+++ ticketing/__init__.py
@@ -5,5 +5,5 @@ Ticketing
 
 import pkg_resources
 
-VERSION = tuple(map(int, pkg_resources.get_distribution('ticketing').version.split('.')))
+VERSION = tuple(map(int, pkg_resources.get_distribution('django-ticketing').version.split('.')))
 __version__ = VERSION
